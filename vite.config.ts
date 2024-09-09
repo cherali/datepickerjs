7c6e184517111f9decc75a5cacadb0cb2807fa75
Ali.Cheraghi <alicheraghi148@gmail.com>
2024-09-09 11:28:33 +0330

    add build optimization

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from "vitest/config";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
 import eslint from "vite-plugin-eslint";
+import esbuild from "rollup-plugin-esbuild";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts({}), eslint()],
+  plugins: [
+    dts({}),
+    eslint(),
+    esbuild({
+      minify: true,
+      legalComments: "none",
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src"),
